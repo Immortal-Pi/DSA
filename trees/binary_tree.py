846fc9ab72d9409837bf06d5dfb4a37bf285da3b
amruth pai <26.amruth@gmail.com>
2026-07-27 21:24:50 -0400

    tree

---
+++ trees/binary_tree.py
@@ -0,0 +1,52 @@
+class BinaryTree:
+    def __init__(self,val):
+        self.val=val
+        self.left=None 
+        self.right=None 
+
+
+def build_tree(treeStructure):
+    if treeStructure==None:
+        return None 
+    key,value=treeStructure
+    # print(key,value[1])
+
+    root=BinaryTree(key)
+    if len(value)>=1:
+        root.left=build_tree(value[0])
+
+    if len(value)>=2:
+        root.right=build_tree(value[1])
+    return root
+
+
+def print_tree(root):
+    if root==None:
+        return root 
+
+    print(root.val,end=':')
+    if root.left!=None:
+        print(root.left.val,end=',')
+    else:
+        print(f'None',end=',')
+    if root.right!=None:
+        print(root.right.val,end=',')
+    else:
+        print(f'None',end=',')
+    print('\n')
+    print_tree(root.left)
+    print_tree(root.right)
+
+tree1_struture=(1,[
+    (2,[
+        (4,[]),
+        (5,[])
+        ]),
+    (3,[
+        (6,[])
+    ])
+])
+
+if __name__=='__main__':
+    tree1=build_tree(tree1_struture)
+    print_tree(tree1)
